b2f8dfec305f8a4e5f02373ed3667069634ce115
Kinsi <kinsi55@users.noreply.github.com>
2021-06-18 22:02:16 +0200

    Space

--- UI/SongListController.cs
+++ UI/SongListController.cs
@@ -210,7 +210,7 @@ namespace BetterSongSearch.UI {
 		}
 
 		public string fullFormattedSongName => $"<color=#{(CheckIsDownloaded() ? "888" : "FFF")}>{detailsSong.songAuthorName} - {detailsSong.songName}</color>";
-		public string uploadDateFormatted => detailsSong.uploadTime.ToString(" dd. MMM yyyy", new CultureInfo("en-US"));
+		public string uploadDateFormatted => detailsSong.uploadTime.ToString("dd. MMM yyyy", new CultureInfo("en-US"));
 		public string songLength => detailsSong.songDuration.ToString("mm\\:ss");
 		public string songRating => showVotesInsteadOfRating ? $"👍 {detailsSong.upvotes} 👎 {detailsSong.downvotes}" : $"{detailsSong.rating:0.0%}";
 
