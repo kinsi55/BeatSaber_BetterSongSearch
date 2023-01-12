16b9fafd103642d036aca0bcd243094edf5edcc6
Kinsi <kinsi55@users.noreply.github.com>
2023-01-12 03:33:48 +0100

    Fix softlock in MP when song starts while you are in BSS

---
+++ HarmonyPatches/HookMpSongStart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BetterSongSearch.UI;
+using HarmonyLib;
+
+namespace BetterSongSearch.HarmonyPatches {
+	[HarmonyPatch(typeof(MultiplayerLevelScenesTransitionSetupDataSO), nameof(MultiplayerLevelScenesTransitionSetupDataSO.Init))]
+	static class HookMpSongStart {
+		static void Prefix() => BSSFlowCoordinator.Close(true, false);
+	}
+}
